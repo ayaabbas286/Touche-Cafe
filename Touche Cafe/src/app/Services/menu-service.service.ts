8768943a05ffade838431b27f1121eeda978fca2
ayaabbas286 <ayaabbas286@gmail.com>
2026-02-04 11:34:53 +0200

    @a carousel home

--- Touche Cafe/src/app/Services/menu-service.service.ts
+++ Touche Cafe/src/app/Services/menu-service.service.ts
@@ -1,14 +1,15 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable, signal,computed } from '@angular/core';
+import { inject, Injectable, signal, computed } from '@angular/core';
 import { map, shareReplay } from 'rxjs';
-import {toSignal} from '@angular/core/rxjs-interop'
+import { toSignal } from '@angular/core/rxjs-interop'
 export interface MenuItem {
   id: number;
   name: string;
   price: number;
-  category:string;
+  category: string;
   description: string;
-  image:string;
+  image: string;
+  featured?: boolean;
 }
 @Injectable({
   providedIn: 'root',
@@ -19,17 +20,22 @@ export class MenuServiceService {
 
   private menuData$ = this._httpClient.get<MenuItem[]>(this.MenuUrl).pipe(shareReplay(1));
 
-  MenuItems = toSignal(this.menuData$, {initialValue: []});
+  MenuItems = toSignal(this.menuData$, { initialValue: [] });
+
+  FeaturedMenu = computed(() => {
+    return this.MenuItems().filter(item => item.featured);
+  });
 
   selectedCategory = signal<string>('mains');
 
   filteredMenu = computed(() => {
-  const items = this.MenuItems();
-  const category = this.selectedCategory();
-  return items.filter(item => item.category === category);
-});
+    const items = this.MenuItems();
+    const category = this.selectedCategory();
+    return items.filter(item => item.category === category);
+  });
 
-setCategory(category:string){
-  this.selectedCategory.set(category);
-}
+  setCategory(category: string) {
+    this.selectedCategory.set(category);
+  }
+
 }
