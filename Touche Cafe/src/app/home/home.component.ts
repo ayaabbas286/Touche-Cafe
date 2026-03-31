7e22f28220064004afc36733e8e3d62fbfeadefe
Moustafa Mahmoud <moustafamahmoud5600@gmail.com>
2026-03-31 19:48:19 +0200

    @M

--- Touche Cafe/src/app/home/home.component.ts
+++ Touche Cafe/src/app/home/home.component.ts
@@ -1,36 +1,67 @@
-import { MenuServiceService } from './../Services/menu-service.service';
+import {
+  MenuItem,
+  MenuServiceService,
+} from './../Services/menu-service.service';
 import { CommonModule } from '@angular/common';
-import { Component, inject  } from '@angular/core';
+import { Component, HostListener, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-home',
-    imports: [CommonModule ],
-    templateUrl: './home.component.html',
-    styleUrl: './home.component.css'
+  selector: 'app-home',
+  imports: [CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
- menuService = inject(MenuServiceService);
- private router = inject(Router);
- private route = inject (ActivatedRoute)
+  menuService = inject(MenuServiceService);
+  private router = inject(Router);
+  private route = inject(ActivatedRoute);
+
+  constructor() {
+    setTimeout(() => {
+      const fragment = this.route.snapshot.fragment;
+      if (fragment) {
+        this.scrollToFragment(fragment);
+      }
+    }, 1000);
+  }
+  scrollToFragment(fragment: string) {
+    const el = document.getElementById(fragment);
+    if (el) {
+      el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
+  ngOnInit() {
+    this.showimg();
+  }
+  @HostListener('window:resize') onResize() {
+    this.showimg();
+  }
+  // عددالصور في كل سلايد
+  imggro: MenuItem[][] = [];
+  imgnum = 4;
+
+  showimg() {
+    const width = window.innerWidth;
+    const allitems = this.menuService.MenuItems();
+    this.imggro = [];
+
+    if (width <= 767) {
+      this.imgnum = 1;
+    } else if (width > 767 && width <= 991) {
+      this.imgnum = 3;
+    }
+      
+    else {
+      {
+        this.imgnum = 4;
+      }
+    }
+    for (let i = 0; i < allitems.length; i += this.imgnum) {
+      this.imggro.push(allitems.slice(i, i + this.imgnum));
+    }
+  }
 
 
- constructor() {
-  setTimeout(() => {
-    const fragment = this.route.snapshot.fragment;
-if(fragment){
-  this.scrollToFragment(fragment)
 }
-  }, 1000);
-
- }
- scrollToFragment(fragment : string) {
-   const el = document.getElementById(fragment);
-   if(el){
-    el.scrollIntoView({behavior: 'smooth', block: 'start' } );
-   }
- }
-
-}
-
